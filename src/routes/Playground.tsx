c3b3f51adaea736623400dc68a8daf217068e53f
doldam0 <jinustar19@gmail.com>
2023-11-01 21:53:33 +0900

    feat: Add header component

--- src/routes/Playground.tsx
+++ src/routes/Playground.tsx
@@ -9,6 +9,13 @@ export function Playground() {
         Click me
       </Button>
       <Button onClick={() => console.log('clicked')}>Click me</Button>
+      <h2>Header</h2>
+      <h1>Header 1</h1>
+      <h2>Header 2</h2>
+      <h3>Header 3</h3>
+      <h4>Header 4</h4>
+      <h5>Header 5</h5>
+      <h6>Header 6</h6>
     </div>
   );
 }
